6b013a1ab9515ff90b79f2d867124ca99e2a2d87
Spoon4 <spoon@imac-de-spoon.home>
2013-12-04 11:58:23 +0100

    bin/lightsaml generates an error issue

--- bin/lightsaml.php
+++ bin/lightsaml.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 
-require_once '../autoload.php';
+require_once __DIR__ . '/../autoload.php';
 
 $app = new \Symfony\Component\Console\Application('lightsaml', '1.0');
 $input = new \Symfony\Component\Console\Input\ArgvInput();
